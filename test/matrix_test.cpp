7c981e62ac750fe964ffc37b775d5a594f2359ee
Ditt Namn <johanjoensson@gmail.com>
2023-02-06 22:52:26 +0900

    Minor updates to tests

--- test/matrix_test.cpp
+++ test/matrix_test.cpp
@@ -107,9 +107,9 @@ TEST(Matrix, TestScale3)
 
 
     auto n = m/2;
-    // m1[i, j] = 2*i + j + 1
-    // m1[0, 0] = 1, m[0, 1] = 2
-    // m1[1, 0] = 3, m1[1, 1] = 4
+    // m[i, j] = 2*i + j + 1
+    // m[0, 0] = 1, m[0, 1] = 2
+    // m[1, 0] = 3, m[1, 1] = 4
     for(size_t i = 0; i < m.rows; i++){
         for(size_t j = 0; j < m.cols; j++){
             int val = n[i, j];
